85f123bb896a2377401efa13309d8081faa24e9f
Mattson Compton <mcompton@Mattsons-MacBook-Pro.local>
2019-02-04 20:24:46 -0700

    finished project

---
+++ index.js
@@ -0,0 +1,18 @@
+// function growAnimation(){
+//     let growEl = document.getElementById('grow')
+//     if(growEl.classList.contains('grow-animation')){
+//         growEl.classList.remove('grow-animation')
+//     }
+//     else{
+//         growEl.classList.add('grow-animation')
+//     }
+//  }
+
+function myFunction(){
+    let showEl = document.getElementById("dropdown-id")
+    if(showEl.classList.contains('show')){
+        showEl.classList.remove('show')
+    } else{
+        showEl.classList.add('show')
+    }
+}
